9d9d86e483c62a3383983178953f5ada9eb19825
Gabrieal4Real <danieljayarajan@gmail.com>
2020-08-10 23:25:36 +0800

    Committing

--- app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
@@ -1,8 +1,7 @@
 package com.smartherd.msgshareapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -13,16 +12,10 @@ class SecondActivity : AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         val message =  bundle!!.getString("user_name")
 
-        for(i in 0..5)
-        {
-            println(message + i)
-            println(message + i)
-            println(message + i)
-            println(message + i)
+        for (i in 0..5) {
             println(message + i)
             nameshowtext.text = "Hello " + message
             //hello
         }
-        println(message)
     }
 }
