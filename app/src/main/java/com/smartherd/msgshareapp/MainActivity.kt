7dca2cea3ffe838e0797da138d576b45093154df
Gabrieal4Real <danieljayarajan@gmail.com>
2020-08-10 23:37:44 +0800

    Changes to Main

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.smartherd.msgshareapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,16 +26,30 @@ class MainActivity : AppCompatActivity() {
 
             if(message.equals(""))
             {
-                Toast.makeText(this,"Please Enter Name", Toast.LENGTH_SHORT).show()
-            }
-
-            else
-            {
-                Toast.makeText(this,"Welcome " + message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please Enter Message 1", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Welcome " + message, Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, SecondActivity::class.java)
                 intent.putExtra("user_name", message)
                 startActivity(intent)
             }
+        }
+
+        sharetootherapp.setOnClickListener {
+            Log.d("", "Share Button Clicked!")
+            val message: String = edittextnextactivity.text.toString()
+
+            if (message.equals("")) {
+                Toast.makeText(this, "Please Enter Message 2", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                startActivity(Intent.createChooser(intent, "Share to: "))
+            }
+
+
         }
     }
 }
